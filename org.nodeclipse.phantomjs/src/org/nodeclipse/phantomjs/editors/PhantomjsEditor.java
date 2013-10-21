17e6465721cdd3194911fccc1af9374e01a78133
Paul Verest <paul.verest@live.com>
2013-10-21 15:50:08 +0800

    commit hanging changes

---
+++ org.nodeclipse.phantomjs/src/org/nodeclipse/phantomjs/editors/PhantomjsEditor.java
@@ -0,0 +1,18 @@
+package org.nodeclipse.phantomjs.editors;
+
+//import org.eclipse.wst.jsdt.internal.ui.javaeditor.CompilationUnitEditor;
+
+//import org.nodeclipse.ui.editors.NodeEditor;
+
+public class PhantomjsEditor{
+//public class PhantomjsEditor extends NodeEditor{ //The hierarchy of the type PhantomjsEditor is inconsistent
+
+//@SuppressWarnings("restriction")
+//public class PhantomjsEditor extends CompilationUnitEditor{	////The hierarchy of the type PhantomjsEditor is inconsistent
+
+	//into MANIFEST.MF
+//	,
+//	 org.eclipse.wst.jsdt.core;bundle-version="1.1.102",
+//	 org.eclipse.wst.jsdt.ui;bundle-version="1.1.102"	
+
+}
