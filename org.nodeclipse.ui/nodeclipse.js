7bc470e855bdf3f304212cd3acd234e0ce005e01
Paul Verest <paul.verest@live.com>
2013-12-02 12:39:54 +0800

    prepare for 0.8.0 release

--- org.nodeclipse.ui/nodeclipse.js
+++ org.nodeclipse.ui/nodeclipse.js
@@ -1,2 +1,8 @@
-var c = require('./templates/copier.js');
-//console.dir(c);
+
+var fs= require('fs');
+fs.exists('./.project', function (exists) {
+	if (exists)
+		console.log(".project file already exists!");
+		return;
+	var c = require('./templates/copier.js');
+});
