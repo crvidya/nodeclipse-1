a5fd89fd20505afc69da723b4c24ddfc3ca51d5c
Paul Verest <paul.verest@live.com>
2014-02-17 17:52:12 +0800

    OS does not change while Eclipse is running

--- org.nodeclipse.ui/src/org/nodeclipse/ui/util/OSUtils.java
+++ org.nodeclipse.ui/src/org/nodeclipse/ui/util/OSUtils.java
@@ -2,13 +2,16 @@ package org.nodeclipse.ui.util;
 
 import org.eclipse.core.runtime.Platform;
 
+/**
+ * @author Nodeclipse authors
+ * @author Paul Verest
+ */
 public class OSUtils {
 
-    private OSUtils() {
-    }
-
+	public static final boolean isWindows = Platform.getOS().startsWith("win");
+	
     public static boolean isWindows() {
-        return Platform.getOS().startsWith("win");
+        return isWindows;
     }
 
     public static boolean isMacOS() {
